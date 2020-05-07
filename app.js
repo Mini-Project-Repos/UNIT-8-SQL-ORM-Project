9b84d10ca2ce79a3974a63ce379df740bea74557
Jamie Gobeille <djent34@gmail.com>
2020-05-07 13:57:34 +0900

    add validations and constraints to model

--- app.js
+++ app.js
@@ -17,14 +17,30 @@ const { Movie } = db.models;
   await db.sequelize.sync({ force: true });
   try {
     //Array of movie data
-    const movies = [{ title: "Toy Story" }, { title: "The Incredibles" }];
+    const movies = [
+      {
+        title: "Toy Story",
+        runtime: 81,
+        releaseDate: "1995-11-22",
+        isAvailableOnVHS: true,
+      },
+      {
+        title: "The Incredibles",
+        runtime: 115,
+        releaseDate: "2004-04-14",
+        isAvailableOnVHS: true,
+      },
+    ];
     /**
      * Movie.create() builds a new model instance, which represents a database row, and automatically stores the instance's data.
      * It returns a Promise object, which resolves or rejects based on the successful or failed interaction with your database.
      */
     // Instance of the Movie class represents a database row
     //create movies
-    const movieCreations = movies.map(({ title }) => Movie.create({ title }));
+    const movieCreations = movies.map(
+      //maps through each object in the array, then destructures it
+      (movie) => Movie.create({ ...movie })
+    );
     await Promise.all(movieCreations);
     // database connection testing
 
--- db/models/movie.js
+++ db/models/movie.js
@@ -21,8 +21,61 @@ module.exports = (sequelize) => {
   Movie.init(
     //Arg1: model attributes
     {
-      //signifies that title entered will be a string
-      title: Sequelize.STRING,
+      id: {
+        type: Sequelize.INTEGER,
+        //The ID acts as a 'primary key', or a unique indexable reference for each entry.
+        primaryKey: true, // true intructs Sequelize to generate the primary key column using the property name defined in the model (in this case it's id, but it could be anything, like identifier). The ID should be a number, so its data type is INTEGER,
+        autoIncrement: true, //true automatically generates an ID that increments by 1 for each new entry
+      },
+      title: {
+        type: Sequelize.STRING,
+        allowNull: false, //disallow Null
+        validate: {
+          notEmpty: {
+            // custom error message
+            msg: 'Please provide a value for "title"',
+          },
+          notNull: {
+            msg: 'Please provide a value for "title"',
+          },
+        },
+      },
+      runtime: {
+        type: Sequelize.INTEGER,
+        allowNull: false, //disallow Null
+        validate: {
+          notNull: {
+            msg: 'Please provide a value for "runtime"',
+          },
+          min: {
+            /**
+             * args represents the value (argument) passed to the validator.
+             * The value you specify is used to check if a column value is valid or invalid.
+             */
+            args: 1,
+            msg: `Please provide a value greater than "0" for "runtime" `,
+          },
+        },
+      },
+      releaseDate: {
+        type: Sequelize.DATEONLY,
+        allowNull: false, //disallow Null
+        validate: {
+          notNull: {
+            msg: 'Please provide a value for "releaseDate"',
+          },
+          isAfter: {
+            args: "1895-12-27",
+            msg:
+              'Please provide a value on or after "1895-12-28" for "releaseDate"',
+          },
+        },
+      }, // yyyy-mm-dd
+      isAvailableOnVHS: {
+        type: Sequelize.BOOLEAN,
+        allowNull: false, //disallow Null
+        defaultValue: false, // set default value
+      },
     },
     //arg2: model options
     { sequelize } //same as {sequelize: sequelize}
